4eed1badd45a6491358dec84d157a152b157ab7e
kishanmoliya <kishanmoliya232@gmail.com>
2021-12-18 13:19:23 +0530

    .

--- C programing/Simple program/aeriaoftriangle.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	float hight=5,base=5;
-	printf("The area of triangle is %f",0.5*hight*base);
-	return 0;
-}
--- C programing/Simple program/simpleinterest.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	float principal=1.5,rate=500,month=2;
-	printf("The simple interest is %f",principal*rate*month/100);
-}
