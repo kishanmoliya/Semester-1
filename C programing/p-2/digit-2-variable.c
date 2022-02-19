709edf10836727c48da0d8f47a88024c219bcff7
kishanmoliya <kishanmoliya232@gmail.com>
2022-02-19 08:06:13 +0530

    Update digit-2-variable.c

--- C programing/p-2/digit-2-variable.c
+++ C programing/p-2/digit-2-variable.c
@@ -1,75 +1,58 @@
-// #include<stdio.h>
-// void main()
-// {
-//     int a,b,suma=0,sumb=0;
-
-//     printf("Enter the value of a or b = \n");
-//     scanf("%d\n,%d",&a,&b);
-
-//     while(a>0)
-//     {
-//         suma=suma+a%10;
-//         a/=10;
-//     }
-//     while(b>0)
-//     {
-//         sumb=sumb+b%10;
-//         b/=10;
-//     }
-
-//     if(suma>sumb)
-//     printf("a is larjest\n");
-
-//     else if(suma==sumb)
-//     printf("-1\n");
-
-//     else
-//     printf("b is larjest");
-// }
-
 #include<stdio.h>
-int sumofdigit(int)
-int checksum(int, int)
-void main()
+int sumofdigit(int);
+int compare(int,int);
+
+int sum(int a)
 {
-    int x,y,z1,z2,check;
-
-    printf("Enter the value of x or y = \n");
-    scanf("%d\n,%d",&x,&y);
-
-    z1=sumofdigit(x);
-    z2=sumofdigit(y);
-
-    check = checksum(z1,z2);
-    printf("%d",check);
+    int j,s;
+    for(s=0;a>0;a=a/10)
+    {
+        j=a%10;
+        s=s+j;
+    }
+    return s;  
+}
+int compare(int x,int y)
+{
+    if(x==y)
+    {
+        return -1;
+    }
+    else if(x>y)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-    int sumofdigit(int n)
-    {
-        int i,a,sum;
-        for(i=1;n!=0;i++)
-        {
-            a=n%10;
-            n=n/10;
-            sum=sum+a;
-        }
-        return sum;
-    }
+void main()
+{
+    int sa,sb,a,b,c;
 
-    int checksum(int z1,int z2)
+    printf("Enter the value of a = ");
+    scanf("%d",&a);
+    printf("Enter the value of b = ");
+    scanf("%d",&b);
+
+    sa=sum(a);
+    sb=sum(b);
+
+    c=compare(sa,sb);
+
+    if(c==-1)
     {
-        if(z1==z2)
-        {
-        printf("-1\n");
-        }
-        
-        else if(z1>z2)
-        {
-            printf("a is larjest");
-        }
-        else
-        {
-            printf("b is larjest");
-        }
+        printf("%d",c);
     }
- 
+    else if(c==1)
+    {
+        printf("%d",a);
+    }
+    else
+    {
+        printf("%d",b);
+    }
+}
+
